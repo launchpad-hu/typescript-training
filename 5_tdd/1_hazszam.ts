c8b14f3acb0759daac58ef6289511f31cc231c2a
Bernát Kalló <kallo.bernat@gmail.com>
2023-04-23 18:03:12 +0200

    5

---
+++ 5_tdd/1_hazszam.ts
@@ -0,0 +1,8 @@
+/*
+ készíts egy függvényt, ami egy sztringként megadott címből
+ kinyeri és visszaadja a házszámot!
+
+ Például
+ hazszam("Futrinka utca 2.")  => 2
+ hazszam("Üllői út 112")  => 112
+ */
